1d30fe2deb6b3f106a91ae3cee03f700ec969552
Wagner Cardoso <fisica.br@gmail.com>
2021-05-05 20:49:04 -0300

    tutorial do app de votacao

--- django/poll_project/poll_project/asgi.py
+++ django/poll_project/poll_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for escola project.
+ASGI config for poll_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escola.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poll_project.settings')
 
 application = get_asgi_application()
--- django/poll_project/poll_project/wsgi.py
+++ django/poll_project/poll_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for escola project.
+WSGI config for poll_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escola.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poll_project.settings')
 
 application = get_wsgi_application()
